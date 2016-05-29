bdd9c35aa3962ca16ffaf8a126dbddc9fb344915
Joe Schweitzer <joe.schweitzer@dataworks-inc.com>
2016-05-29 00:13:59 -0400

    Added ES basic search and test

--- app/test/server/server.js
+++ app/test/server/server.js
@@ -10,7 +10,23 @@ describe("Applicant Server", function() {
           expect(response.statusCode).to.equal(200);
 
           var data = JSON.parse(body);
-          expect(data).to.have.length(2);
+          expect(data).to.have.length(3);
+          expect(data[0].name).to.equal('Joe Schweitzer');
+
+          done();
+        });
+      });
+    });
+
+    describe("Search", function() {
+      var url = "http://localhost:8082/search";
+
+      it("returns dummy applicants", function(done) {
+        request(url, function(error, response, body) {
+          expect(response.statusCode).to.equal(200);
+
+          var data = JSON.parse(body);
+          expect(data).to.have.length(3);
           expect(data[0].name).to.equal('Joe Schweitzer');
 
           done();
