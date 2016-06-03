76faa884f25a0f4ee5d1eead17ec4f3553d25297
Brantley E. Gilbert <brantley.gilbert@dataworks-inc.com>
2016-06-03 11:34:06 -0400

    Changed TikaFunc in ResumeReader to use PortableDataStream instead of the file path.  This will allow it to work with non-local file systems (HDFS, S3)

--- etl/src/scala/applicant/etl/ResumeReader.scala
+++ etl/src/scala/applicant/etl/ResumeReader.scala
@@ -20,28 +20,21 @@ import java.io._
 
 object ResumeReader {
 
-  def tikaFunc (a: (String, PortableDataStream)) = {
+  def tikaFunc (a: PortableDataStream) = {
 
-    val file : File = new File(a._1.drop(5))
+    //val file : File = new File(a._1.drop(5))
     val myparser : AutoDetectParser = new AutoDetectParser()
-    val stream : InputStream = new FileInputStream(file)
+    val stream : InputStream = new DataInputStream(a.open())
     val handler : WriteOutContentHandler = new WriteOutContentHandler(-1)
     val metadata : Metadata = new Metadata()
     val context : ParseContext = new ParseContext()
-
     myparser.parse(stream, handler, metadata, context)
 
     stream.close
 
     println(handler.toString())
     println("---------------------------------------------")
-    /*
-    //Write results to file
-    val outputfile = new File("~/")
-    val bw = new BufferedWriter(new FileWriter(outputfile))
-    bw.write(handler.toString())
-    bw.close()
-    */
+
   }
 
 
@@ -61,7 +54,6 @@ object ResumeReader {
     val conf = new SparkConf().setMaster(spark_master).setAppName("ResumeReader")
     val sc = new SparkContext(conf)
     val fileData = sc.binaryFiles(filesPath)
-    fileData.foreach( x => tikaFunc(x))
-
+    fileData.values.foreach( x => tikaFunc(x))
   }
 }
