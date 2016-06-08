8410ad5175faec416c746c335730f52791796935
William Goubeaux <goubew@users.noreply.github.com>
2016-06-08 14:17:54 -0400

    Added rest of cases

--- etl/src/scala/applicant/etl/EntityMapper.scala
+++ etl/src/scala/applicant/etl/EntityMapper.scala
@@ -15,6 +15,7 @@ object EntityMapper {
   def createMap(taggedEntities: LinkedHashSet[(String, String)], applicantID: String, fullText: String): Map[String, Object] = {
     var name, score, recentTitle, recentLocation, recentOrganization, degree, school, gpa, url, email, phone: String = "not found"
     val languageList: ListBuffer[String] = new ListBuffer[String]()
+    val bigDataList: ListBuffer[String] = new ListBuffer[String]()
     val etlList: ListBuffer[String] = new ListBuffer[String]()
     val databaseList: ListBuffer[String] = new ListBuffer[String]()
     val webappList: ListBuffer[String] = new ListBuffer[String]()
@@ -37,7 +38,14 @@ object EntityMapper {
           otherOrganizationList += pair._2
         case ("person", _) if (name == "not found") => name = pair._2
         case ("school", _) if (school == "not found") => school = pair._2
-
+        case ("title", _) => if (recentTitle == "not found") { recentTitle = pair._2 }
+          otherTitleList += pair._2
+        case ("bigdata", _) => bigDataList += pair._2
+        case ("database", _) => databaseList += pair._2
+        case ("etl", _) => etlList += pair._2
+        case ("webapp", _) => webappList += pair._2
+        case ("mobile", _) => mobileList += pair._2
+        case ("language", _) => languageList += pair._2
         case ("gpa", _) if (gpa == "not found") => gpa = pair._2
         case ("email", _) if (email == "not found") => email = pair._2
         case ("phone", _) if (phone == "not found") => phone = pair._2
@@ -55,6 +63,7 @@ object EntityMapper {
       ),
       "skills" -> Map(
           "langage" -> languageList,
+          "bigdata" -> bigDataList,
           "etl" -> etlList,
           "database" -> databaseList,
           "webapp" -> webappList,
