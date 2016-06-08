55a868fb5e22d7cf4fe7348e87c8125640bf1d7b
nsudini <nsudini@vt.edu>
2016-06-08 10:35:25 -0400

    trying to connect elasticsearch

--- app/server/server.js
+++ app/server/server.js
@@ -8,31 +8,45 @@ app.use(express.static("client"));
 app.use(express.static("node_modules"));
 
 app.get("/service/applicants", function(req, res) {
-  res.json([{
-    "name": "Joe Schweitzer",
-    "score": 0.99,
-    "currentEntity": "Data Works Inc.",
-    "currentLocation": "Reston, VA",
-    "skills": ["Grails", "Groovy", "Ext JS"]
-  }, {
-    "name": "Laura Schweitzer",
-    "score": 0.98,
-    "currentEntity": "Data Works Inc.",
-    "currentLocation": "Reston, VA",
-    "skills": ["Pentaho", "Ruby on Rails", "Javascript"]
-  }, {
-    "name": "Dave Mezzetti",
-    "score": 0.30,
-    "currentEntity": "Data Works Inc.",
-    "currentLocation": "Reston, VA",
-    "skills": ["Scala", "Spark", "Tweeting"]
-  }, {
-    "name": "Dennis Schweitzer",
-    "score": 0.58,
-    "currentEntity": "Data Works Inc.",
-    "currentLocation": "Reston, VA",
-    "skills": ["Pentaho", "Ruby on Rails", "Javascript"]
-  }]);
+  var client = new elasticsearch.Client({
+    host: '172.31.61.189:9200'
+  });
+
+  client.search({
+    index: 'sample_json',
+    q: 'type:applicant'
+  }).then(function (body) {
+    var hits = body.hits.hits.map(function(hit) { return hit._source; });;
+    res.json(hits);
+  }, function (error) {
+    console.trace(error.message);
+  });
+
+  // res.json([{
+  //   "name": "Joe Schweitzer",
+  //   "score": 0.99,
+  //   "currentEntity": "Data Works Inc.",
+  //   "currentLocation": "Reston, VA",
+  //   "skills": ["Grails", "Groovy", "Ext JS"]
+  // }, {
+  //   "name": "Laura Schweitzer",
+  //   "score": 0.98,
+  //   "currentEntity": "Data Works Inc.",
+  //   "currentLocation": "Reston, VA",
+  //   "skills": ["Pentaho", "Ruby on Rails", "Javascript"]
+  // }, {
+  //   "name": "Dave Mezzetti",
+  //   "score": 0.30,
+  //   "currentEntity": "Data Works Inc.",
+  //   "currentLocation": "Reston, VA",
+  //   "skills": ["Scala", "Spark", "Tweeting"]
+  // }, {
+  //   "name": "Dennis Schweitzer",
+  //   "score": 0.58,
+  //   "currentEntity": "Data Works Inc.",
+  //   "currentLocation": "Reston, VA",
+  //   "skills": ["Pentaho", "Ruby on Rails", "Javascript"]
+  // }]);
 });
 
 // possible changes to work with local the host to hosts, added [] and added the local to the host list
