82f8c2787e1ccf959b2da8115ccfab6ca3732a9b
nsudini <nsudini@vt.edu>
2016-06-09 15:19:35 -0400

    added elasticsearch queries

--- app/server/server.js
+++ app/server/server.js
@@ -3,53 +3,71 @@ var elasticsearch = require("elasticsearch");
 var root = express();
 var app = express();
 
+var esservice = require("./elasticsearch.js")
+
+var applicantConfig = {
+  url: "172.31.61.189:9200",
+  index: "sample_json",
+  type: "applicant"
+};
+
 app.use(express.static("client"));
 
 app.use(express.static("node_modules"));
 
 app.get("/service/applicants", function(req, res) {
-//   res.json([{
-//     "name": "Joe Schweitzer",
-//     "score": 0.99,
-//     "currentEntity": "Data Works Inc.",
-//     "currentLocation": "Reston, VA",
-//     "skills": ["Grails", "Groovy", "Ext JS"]
-//   }, {
-//     "name": "Laura Schweitzer",
-//     "score": 0.98,
-//     "currentEntity": "Data Works Inc.",
-//     "currentLocation": "Reston, VA",
-//     "skills": ["Pentaho", "Ruby on Rails", "Javascript"]
-//   }, {
-//     "name": "Dave Mezzetti",
-//     "score": 0.30,
-//     "currentEntity": "Data Works Inc.",
-//     "currentLocation": "Reston, VA",
-//     "skills": ["Scala", "Spark", "Tweeting"]
-//   }, {
-//     "name": "Dennis Schweitzer",
-//     "score": 0.58,
-//     "currentEntity": "Data Works Inc.",
-//     "currentLocation": "Reston, VA",
-//     "skills": ["Pentaho", "Ruby on Rails", "Javascript"]
-//   }]);
+
+  // var query = "name:Dave Mezzetti";
+  esservice.query(labelConfig, req, res, "*", function(res, hits){
+    //var ids = map source -> _id
+
+    //same query logic * or NOT id ()
+
+    esservice.query(applicantsConfig, req, res, query, null);
+  });
+
+  // res.json([{
+  //   "name": "Joe Schweitzer",
+  //   "score": 0.99,
+  //   "currentEntity": "Data Works Inc.",
+  //   "currentLocation": "Reston, VA",
+  //   "skills": ["Grails", "Groovy", "Ext JS"]
+  // }, {
+  //   "name": "Laura Schweitzer",
+  //   "score": 0.98,
+  //   "currentEntity": "Data Works Inc.",
+  //   "currentLocation": "Reston, VA",
+  //   "skills": ["Pentaho", "Ruby on Rails", "Javascript"]
+  // }, {
+  //   "name": "Dave Mezzetti",
+  //   "score": 0.30,
+  //   "currentEntity": "Data Works Inc.",
+  //   "currentLocation": "Reston, VA",
+  //   "skills": ["Scala", "Spark", "Tweeting"]
+  // }, {
+  //   "name": "Dennis Schweitzer",
+  //   "score": 0.58,
+  //   "currentEntity": "Data Works Inc.",
+  //   "currentLocation": "Reston, VA",
+  //   "skills": ["Pentaho", "Ruby on Rails", "Javascript"]
+  // }]);
 // });
 
 // // possible changes to work with local the host to hosts, added [] and added the local to the host list
 // app.get("/service/search", function(req, res) {
-  var client = new elasticsearch.Client({
-    host: 'interns.dataworks-inc.com/elasticsearch'
-  });
+  // var client = new elasticsearch.Client({
+  //   host: 'interns.dataworks-inc.com/elasticsearch'
+  // });
 
-  client.search({
-    index: 'sample_json',
-    q: req.params.query || '*'
-  }).then(function (body) {
-    var hits = body.hits.hits.map(function(hit) { return hit._source; });;
-    res.json(hits);
-  }, function (error) {
-    console.trace(error.message);
-  });
+  // client.search({
+  //   index: 'sample_json',
+  //   q: req.params.query || '*'
+  // }).then(function (body) {
+  //   var hits = body.hits.hits.map(function(hit) { return hit._source; });;
+  //   res.json(hits);
+  // }, function (error) {
+  //   console.trace(error.message);
+  // });
 });
 
 root.get("/", function(req, res) {
