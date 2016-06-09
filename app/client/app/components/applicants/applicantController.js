4ad453a38bde4c8ca8db71060c5accd2b34bc25d
Dennis L. Huang <dlh4fx@virginia.edu>
2016-06-09 16:23:29 -0400

    started return function

--- app/client/app/components/applicants/applicantController.js
+++ app/client/app/components/applicants/applicantController.js
@@ -1,10 +1,14 @@
-applicantControllers.controller('ApplicantCtrl', ['$scope', 'Applicant', 'Favorite',
+applicantControllers.controller('ApplicantCtrl', ['$scope', 'Applicant', 'Favorite', 
   function ($scope, Applicant, Favorite) {
     $scope.applicants = Applicant.query();
     $scope.mark = function (id, type) {
     	var favorite = new Favorite({'id': id, 'type' : type});
 		favorite.$save();
     };
+    // $scope.ret = function (id, type) {
+    // 	var retObject = new RetObject({'id': id, 'type' : type});
+    // 	retObject.$save();
+    // }
   }]);
 
 
