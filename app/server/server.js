402d5f2e1e103e5843c8b0ef7c8f08db6c577a8d
Dennis L. Huang <dlh4fx@virginia.edu>
2016-06-10 14:08:00 -0400

    fixed favorite page

--- app/server/server.js
+++ app/server/server.js
@@ -34,15 +34,14 @@ app.get("/service/applicants", function(req, res) {
   esservice.query(labelConfig, req, res, "*", function(res, hits){
     //var ids = map source -> _id
     var query = '*';
-    if(hits && hits.length > 0) {
-      var ids = hits.hits.map(function(hit) { return hit._source.id; })
+    if (hits && hits.length > 0) {
+      var ids = hits.map(function(hit) { return hit.id; })
 
       //same query logic * or NOT id ()
-      if (ids &&ids.length > 0) {
+      if (ids && ids.length > 0) {
         query = "NOT id:(" + ids.join(",") + ")"
       }
    } 
-   console.log("Query = " + query);
 
     esservice.query(applicantConfig, req, res, query, null);
   });
@@ -51,7 +50,7 @@ app.get("/service/applicants", function(req, res) {
   console.log(error);
 });
 
-//code for favorites
+//code for favorites, changes between favorite and archive--REQUIRED
 app.post("/service/favorites", function(req, res) {
   var client = new elasticsearch.Client({
     host: 'interns.dataworks-inc.com/elasticsearch'
@@ -76,10 +75,21 @@ app.post("/service/favorites", function(req, res) {
 
 //get code for favorites
 app.get("/service/favorites", function(req, res) {
+  // var query = "name:Dave Mezzetti";
+  esservice.query(labelConfig, req, res, "type: favorite", function(res, hits){
+    //var ids = map source -> _id
+    var query = '*';
+    if (hits && hits.length > 0) {
+      var ids = hits.map(function(hit) { return hit.id; })
 
- 
+      //same query logic * or NOT id ()
+      if (ids && ids.length > 0) {
+        query = "id:(" + ids.join(",") + ")"
+      }
+   } 
 
-    esservice.query(labelConfig, req, res, "*", null);
+    esservice.query(applicantConfig, req, res, query, null);
+  });
 
 },function (error, response) {
   console.log(error);
