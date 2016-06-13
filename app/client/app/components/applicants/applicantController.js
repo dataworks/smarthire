c7a75cb448e81c513055d648697f46165407f4fb
nsudini <nsudini@vt.edu>
2016-06-13 12:41:37 -0400

    Got dropdown menu to work with angular

--- app/client/app/components/applicants/applicantController.js
+++ app/client/app/components/applicants/applicantController.js
@@ -1,9 +1,8 @@
 applicantControllers.controller('ApplicantCtrl', ['$scope', 'Applicant', 'Favorite', '$location', 'Archive', 
   function ($scope, Applicant, Favorite, $location, Archive) {
-
-
     $scope.applicants = Applicant.query();
-    $scope.filter = function(type) {
+
+    $scope.showSelectValue = function(type) {
         console.log(type);
         if (type == 'Favorite') {
             $scope.applicants = Favorite.query();
@@ -14,12 +13,7 @@ applicantControllers.controller('ApplicantCtrl', ['$scope', 'Applicant', 'Favori
         if (type == 'Applicant') {
             $scope.applicants = Applicant.query();
         }
-
     };
-
-
-    //$scope.fav = Favorite.query();
-    //$scope.archive = Archive.query();
 
     $scope.mark = function (id, type) {
     	var favorite = new Favorite({'id': id, 'type' : type});
