c2d0b8408f0bab19f28a16608a6b1f3675de06fc
Dennis L. Huang <dlh4fx@virginia.edu>
2016-06-13 11:53:57 -0400

    removed unnecessary files and comments

--- app/client/app/components/archive/archiveController.js
+++
@@ -1,4 +0,0 @@
-applicantControllers.controller('ArchiveCtrl', ['$scope', 'Archive',
-  function ($scope, Archive) {
- 	$scope.labels = Archive.query();
-  }]);
--- app/client/app/components/favorites/favoritesController.js
+++
@@ -1,5 +0,0 @@
-applicantControllers.controller('FavoritesCtrl', ['$scope', 'Favorite',
-  function ($scope, Favorite) {
-  	$scope.labels = Favorite.query();
- 
-  }]);
