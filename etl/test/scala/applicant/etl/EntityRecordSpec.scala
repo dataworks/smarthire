c68fb991774fbd1547d7c4db46a8f173d1b6c535
William Goubeaux <goubew@users.noreply.github.com>
2016-06-13 15:21:43 -0400

    Why have 1 test when you can have all the tests?

--- etl/test/scala/applicant/etl/EntityRecordSpec.scala
+++ etl/test/scala/applicant/etl/EntityRecordSpec.scala
@@ -9,29 +9,50 @@ import scala.collection.mutable.{LinkedHashSet, ListBuffer, HashMap}
  *
  */
  class EntityRecordSpec extends FlatSpec {
-   "Create" must "create a map" in {
-     val input: LinkedHashSet[(String, String)] = LinkedHashSet[(String, String)]()
+   val input: LinkedHashSet[(String, String)] = LinkedHashSet[(String, String)]()
 
-     input += ("person" -> "Jason Frederick")
-     input += ("title" -> "Web Developer")
-     input += ("organization" -> "American Financial Group, Inc.")
-     input += ("location" -> "Thousand Oaks, CA")
-     input += ("title" -> "ETL Developer")
-     input += ("organization" -> "Alaska Air Group, Inc.")
-     input += ("location" -> "Columbus, GA")
-     input += ("webapp" -> "JavaScript")
-     input += ("degree" -> "BS Biology")
-     input += ("school" -> "Harvard University")
+   input += ("person" -> "Jason Frederick")
+   input += ("title" -> "Web Developer")
+   input += ("organization" -> "American Financial Group, Inc.")
+   input += ("location" -> "Thousand Oaks, CA")
+   input += ("title" -> "ETL Developer")
+   input += ("organization" -> "Alaska Air Group, Inc.")
+   input += ("location" -> "Columbus, GA")
+   input += ("webapp" -> "JavaScript")
+   input += ("degree" -> "BS Biology")
+   input += ("school" -> "Harvard University")
 
-     val map = EntityRecord.create(input, "Wow what a good resume name", "This is totally the text that gave us these entities :D")
+   val map = EntityRecord.create(input, "Wow what a good resume name", "This is totally the text that gave us these entities :D")
 
+   "EntityRecord" must "store the name" in {
      map.get("name").get mustBe ("Jason Frederick")
+   }
+
+   "EntityRecord" must "store the first title" in {
      map.get("currentLocation").get.asInstanceOf[HashMap[String, String]].get("title") mustBe (Some("Web Developer"))
+   }
+
+   "EntityRecord" must "store the first location" in {
      map.get("currentLocation").get.asInstanceOf[HashMap[String, String]].get("location") mustBe (Some("Thousand Oaks, CA"))
+   }
+
+   "EntityRecord" must "store teh first organization" in {
      map.get("currentLocation").get.asInstanceOf[HashMap[String, String]].get("organization") mustBe (Some("American Financial Group, Inc."))
+   }
+
+   "EntityRecord" must "store teh first degree" in {
      map.get("education").get.asInstanceOf[HashMap[String, String]].get("degree") mustBe (Some("BS Biology"))
+   }
+
+   "EntityRecord" must "store the first school" in {
      map.get("education").get.asInstanceOf[HashMap[String, String]].get("school") mustBe (Some("Harvard University"))
+   }
+
+   "EntityRecord" must "add JavaScript to the webapp list" in {
      map.get("skills").get.asInstanceOf[HashMap[String, ListBuffer[String]]].get("webapp").get.contains("JavaScript") mustBe (true)
+   }
+
+   "EntityRecord" must "store the resume text" in {
      map.get("additionalInfo").get.asInstanceOf[HashMap[String, String]].get("resume") mustBe (Some("This is totally the text that gave us these entities :D"))
-  }
+   }
 }
