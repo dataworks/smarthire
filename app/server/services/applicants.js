808931e554949d0b3a9f0adaad49ea5d2d01f15c
nsudini <nsudini@vt.edu>
2016-06-15 11:08:30 -0400

    more edits to server code

--- app/server/services/applicants.js
+++ app/server/services/applicants.js
@@ -3,10 +3,10 @@ var config = require("./config.js");
 
 //lists applicants based on type
 exports.listApplicants = function(req, res, type) {
-	esservice.query(config.label, req, res, "*", function(res, hits){
+	esservice.query(config.config.labels, req, res, "*", function(res, hits){
     	//var ids = map source -> _id
     	var query = esservice.map(res, hits, type);
-    	esservice.query(configs.applicants, req, res, query, null);
+    	esservice.query(config.config.applicants, req, res, query, null);
   	}, function (error, response) {
         console.log(error);
 	});
--- app/server/services/labels.js
+++ app/server/services/labels.js
@@ -1,6 +1,6 @@
 var esservice = require("./elasticsearch.js");
-var configs = require("./config.js");
+var config = require("./config.js");
 
 exports.index = function(req, res) {
-	esservice.index(config.laels, req, res);
+	esservice.index(config.config.labels, req, res);
 }
