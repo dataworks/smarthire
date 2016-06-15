772b40533e875e71ace8913ced5e2986cf313aa1
Brantley E. Gilbert <brantley.gilbert@dataworks-inc.com>
2016-06-15 15:21:14 -0400

    Broke build.sbt

--- etl/src/scala/applicant/etl/EntityRecord.scala
+++ etl/src/scala/applicant/etl/EntityRecord.scala
@@ -5,7 +5,7 @@ import java.text.DecimalFormat
 import java.net.{URL, HttpURLConnection}
 import scala.io._
 import org.json4s._
-import org.json4s.native.JsonMethods._
+import org.json4s.jackson.JsonMethods._
 
 import scala.collection.mutable.{ListBuffer, Map, LinkedHashMap}
 
