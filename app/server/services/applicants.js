422c9f132e084473384c205b179a0c0f003e08d0
Dennis L. Huang <dlh4fx@virginia.edu>
2016-06-16 14:23:48 -0400

    fixing queries

--- app/server/services/applicants.js
+++ app/server/services/applicants.js
@@ -4,15 +4,14 @@ var config = require("./config.js");
 //lists applicants based on type
 exports.listApplicants = function(req, res, type) {
 	var query = '*';
-	if(type !== 'applicant')
+	if (type !== 'applicant') {
 		query = 'type: ' + type;
+	}
 
-	console.log(query);
-
-	esservice.query(config.config.labels, req, res, query, function(res, hits){
+	esservice.query(config.config.labels, null, res, query, function(res, hits){
     	//var ids = map source -> _id
-	    query = esservice.map(res, hits, type);
-	   	esservice.query(config.config.applicants, req, res, query, null);
+	    var labelQuery = esservice.map(res, hits, type);
+	   	esservice.query(config.config.applicants, req, res, labelQuery, null);
 	}, function (error, response) {
         console.log(error);
 	});
