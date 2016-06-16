bcc19a84325129bc5bf5eef8a839e4348c46ab38
Brantley E. Gilbert <brantley.gilbert@dataworks-inc.com>
2016-06-16 11:00:57 -0400

    Fixed ApiMapper

--- etl/src/scala/applicant/etl/ApiMapper.scala
+++ etl/src/scala/applicant/etl/ApiMapper.scala
@@ -7,7 +7,7 @@ import scala.io._
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
 
-import scala.collection.mutable.{ListBuffer, Map, LinkedHashMap}
+//import scala.collection.mutable.{ListBuffer, Map, LinkedHashMap}
 
 /**
  * Class to retrieve JSON data from URLs and convert them
@@ -17,7 +17,7 @@ import scala.collection.mutable.{ListBuffer, Map, LinkedHashMap}
 object ApiMapper {
   /**
    * Retrieves data from github api and converts it to a map of [String,String]
-   * @param github A string of the parsed applicant's GitHub URL
+   * @param github A string of a GitHub URL
    * @return A [String,String] map of Github data from the Github api
    */
   def githubAPI(github : String) : Map[String,String] = {
@@ -43,10 +43,19 @@ object ApiMapper {
 
     val jsonString = scala.io.Source.fromURL(giturlBuilder.toString()).mkString
     val parsedJson = parse(jsonString)
-    implicit val formats = DefaultFormats
+    object VarToString extends CustomSerializer[String](format => (
+      {
+        case JBool(false) => format.toString
+        case JBool(true) => format.toString
+      },
+      {
+        case format: String => JString(format)
+      }
+    ))
+    implicit val formats = DefaultFormats + VarToString
 
     val gitJsonMap = parsedJson.extract[Map[String, String]]
     println(gitJsonMap)
-    return Map("" -> "")
+    return gitJsonMap
   }
 }
