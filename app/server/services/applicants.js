9ec27cc3eed81bb17f3c00cec68e2498709aab26
nsudini <nsudini@vt.edu>
2016-06-16 15:09:37 -0400

    fixing queries

--- app/server/services/applicants.js
+++ app/server/services/applicants.js
@@ -1,7 +1,9 @@
 var esservice = require("./elasticsearch.js");
 var config = require("./config.js");
 
-//lists applicants based on type
+/*
+*lists applicants based on type
+*/
 exports.listApplicants = function(req, res, type) {
 	var query = '*';
 	if (type !== 'applicant') {
@@ -10,9 +12,29 @@ exports.listApplicants = function(req, res, type) {
 
 	esservice.query(config.config.labels, null, res, query, function(res, hits){
     	//var ids = map source -> _id
-	    var labelQuery = esservice.map(res, hits, type);
+	    var labelQuery = buildQuery(res, hits, type);
 	   	esservice.query(config.config.applicants, req, res, labelQuery, null);
 	}, function (error, response) {
         console.log(error);
 	});
+}
+
+/*
+* Builds the query string for ES
+*/
+function buildQuery(res, hits, type) {
+	console.log(hits);
+    if (hits && hits.length > 0) {
+      var ids = hits.map(function(hit) { return hit.id; });
+      //same query logic * or NOT id ()
+      if (ids && ids.length > 0) { 
+        if(type === 'applicant') {
+          return "NOT id:(" + ids.join(" ") + ")";
+        }
+        else if(type === 'favorite' || type === 'archive' || type === 'review') {
+          return "id:(" + ids.join(" ") + ")";
+        }
+      }
+    }
+    return ' ';
 }
