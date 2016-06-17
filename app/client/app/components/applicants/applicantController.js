143de126f3484f77c88aabda09cc470f61a4f16f
Dennis L. Huang <dlh4fx@virginia.edu>
2016-06-17 10:46:15 -0400

    fixed indentation

--- app/client/app/components/applicants/applicantController.js
+++ app/client/app/components/applicants/applicantController.js
@@ -1,168 +1,198 @@
 applicantControllers.controller('ApplicantCtrl', ['$scope', 'Applicant', 'Favorite', '$location', 'Archive', '$window', 'Review',
-    function ($scope, Applicant, Favorite, $location, Archive, $window, Review) {
+    function($scope, Applicant, Favorite, $location, Archive, $window, Review) {
 
-    $scope.selection = "New";
-    $scope.index = 0;
-    $scope.pageSize = 25;
-    $scope.loadingData = false;
-    $scope.hasData = true;
-
-//sorting table by column code
-$scope.propertyName = null;
-$scope.reverse = false;
-$scope.sortBy = function(propertyName) {
-
-    $scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : false;
-    $scope.propertyName = propertyName;
-};
-
-$scope.applicants = Applicant.query({from: $scope.index, size: $scope.pageSize});
-
-$scope.showSelectValue = function(type) {
-    if (type == 'Favorite') {
-        $scope.index = 0;
-        $scope.hasData = true;
-        $scope.selection = "Favorite";
-        $scope.applicants = Favorite.query({from: $scope.index, size: $scope.pageSize});
-    }
-    if (type == 'Archive') {
-        $scope.index = 0;
-        $scope.hasData = true;
-        $scope.selection = "Archive";
-        $scope.applicants = Archive.query({from: $scope.index, size: $scope.pageSize});
-    }
-    if (type == 'New') {
-        $scope.index = 0;
-        $scope.hasData = true;
         $scope.selection = "New";
-        $scope.applicants = Applicant.query({from: $scope.index, size: $scope.pageSize});
-    }
-    if (type == 'Review'){
         $scope.index = 0;
+        $scope.pageSize = 25;
+        $scope.loadingData = false;
         $scope.hasData = true;
-        $scope.selection = "Review";
-        $scope.applicants = Review.query({from: $scope.index, size: $scope.pageSize});
+
+        //sorting table by column code
+        $scope.propertyName = null;
+        $scope.reverse = false;
+        $scope.sortBy = function(propertyName) {
+
+            $scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : false;
+            $scope.propertyName = propertyName;
+        };
+
+        $scope.applicants = Applicant.query({
+            from: $scope.index,
+            size: $scope.pageSize
+        });
+
+        $scope.showSelectValue = function(type) {
+            if (type == 'Favorite') {
+                $scope.index = 0;
+                $scope.hasData = true;
+                $scope.selection = "Favorite";
+                $scope.applicants = Favorite.query({
+                    from: $scope.index,
+                    size: $scope.pageSize
+                });
+            }
+            if (type == 'Archive') {
+                $scope.index = 0;
+                $scope.hasData = true;
+                $scope.selection = "Archive";
+                $scope.applicants = Archive.query({
+                    from: $scope.index,
+                    size: $scope.pageSize
+                });
+            }
+            if (type == 'New') {
+                $scope.index = 0;
+                $scope.hasData = true;
+                $scope.selection = "New";
+                $scope.applicants = Applicant.query({
+                    from: $scope.index,
+                    size: $scope.pageSize
+                });
+            }
+            if (type == 'Review') {
+                $scope.index = 0;
+                $scope.hasData = true;
+                $scope.selection = "Review";
+                $scope.applicants = Review.query({
+                    from: $scope.index,
+                    size: $scope.pageSize
+                });
+            }
+        };
+
+
+        $scope.dataLoaded = function(result) {
+            if (result.length > 0) {
+                $scope.applicants = $scope.applicants.concat(result);
+            } else {
+                $scope.hasData = false;
+                $scope.index = 0;
+            }
+
+            $scope.loadingData = false;
+        };
+
+        $scope.nextPage = function() {
+            console.log($scope.selection, $scope.index);
+            if ($scope.hasData) {
+                $scope.loadingData = true;
+                $scope.index += $scope.pageSize;
+
+                if ($scope.selection == "New") {
+                    // console.log("1");
+                    Applicant.query({
+                        from: $scope.index,
+                        size: $scope.pageSize
+                    }, $scope.dataLoaded);
+
+                } else if ($scope.selection == "Favorite") {
+                    // console.log(2);
+                    Favorite.query({
+                        from: $scope.index,
+                        size: $scope.pageSize
+                    }, $scope.dataLoaded);
+
+                } else if ($scope.selection == 'Review') {
+                    Review.query({
+                        from: $scope.index,
+                        size: $scope.pageSize
+                    }, $scope.dataLoaded);
+                } else {
+                    Archive.query({
+                        from: $scope.index,
+                        size: $scope.pageSize
+                    }, $scope.dataLoaded);
+
+                }
+
+            }
+        };
+
+
+        // Only enable if the document has a long scroll bar
+        // Note the window height + offset
+        //if ( $(window).height() > $(document).height()) {
+        $('#top-link-block').removeClass('hidden').affix({
+            // how far to scroll down before link "slides" into view
+            offset: {
+                top: 100
+            }
+        });
+        //}
+
+
+        $scope.mark = function(id, type, index) {
+            var favorite = new Favorite({
+                'id': id,
+                'type': type,
+                'index': index
+            });
+            console.log(favorite)
+            favorite.$save().then(function() {
+                // $scope.applicants = Applicant.query();
+                $scope.applicants.splice(index, 1);
+            });
+        };
+
+        $scope.remove = function(id, index) {
+            var place = new Applicant({
+                'id': id
+            });
+            console.log(place);
+            place.$save().then(function() {
+                $scope.applicants.splice(0, 0, place);
+                // $scope.applicants.splice(index, 1);
+            });
+        };
+
+        $scope.button = function(text) {
+            alert(text);
+        }
+
+        //code for toast messages
+        $scope.showToast = function() {
+            console.log("hello");
+            document.getElementById("favToast").style.display = "block";
+            setTimeout($scope.hideToast, 3000);
+        }
+
+        $scope.hideToast = function() {
+            document.getElementById("favToast").style.display = "none";
+        }
+
+        // $scope.ret = function (id, type) {
+        //  var retObject = new RetObject({'id': id, 'type' : type});
+        //  retObject.$save();
+        // }
+
+        //scroll code
+        $(function() {
+            var lastScrollTop = 0,
+                delta = 5;
+            $(window).scroll(function(event) {
+                var st = $(this).scrollTop();
+
+                if (Math.abs(lastScrollTop - st) <= delta)
+                    return;
+
+                if (st > lastScrollTop) {
+                    // downscroll code
+                    //console.log('scroll down');
+                    //console.log($(window).scrollTop());
+                } else {
+                    // upscroll code
+                    // console.log('scroll up');
+                    // console.log($(window).scrollTop());
+                    angular.element("#footer").hide();
+                }
+                lastScrollTop = st;
+            });
+        });
+
+        $(window).scroll(function() {
+            if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+                angular.element("#footer").show();
+            }
+        });
     }
-};
-
-
-$scope.dataLoaded = function(result) {
-    if (result.length > 0) {
-        $scope.applicants = $scope.applicants.concat(result);
-    }
-    else {
-        $scope.hasData = false;
-        $scope.index = 0;
-    }
-
-    $scope.loadingData = false;
-};
-
-$scope.nextPage = function(){
-    console.log($scope.selection, $scope.index);
-    if ($scope.hasData) {
-        $scope.loadingData = true;
-        $scope.index += $scope.pageSize;
-
-        if ($scope.selection == "New") {
-// console.log("1");
-Applicant.query({from: $scope.index, size: $scope.pageSize}, $scope.dataLoaded);
-
-}
-
-else if ($scope.selection == "Favorite") {
-// console.log(2);
-Favorite.query({from: $scope.index, size: $scope.pageSize}, $scope.dataLoaded);
-
-}  
-
-else if ($scope.selection == 'Review'){
-    Review.query({from: $scope.index, size: $scope.pageSize}, $scope.dataLoaded);
-}
-
-else {
-    Archive.query({from: $scope.index, size: $scope.pageSize}, $scope.dataLoaded);
-
-}
-
-}
-};
-
-
-// Only enable if the document has a long scroll bar
-// Note the window height + offset
-//if ( $(window).height() > $(document).height()) {
-    $('#top-link-block').removeClass('hidden').affix({
-// how far to scroll down before link "slides" into view
-offset: {top:100}
-});
-//}
-
-
-$scope.mark = function (id, type, index) {
-    var favorite = new Favorite({'id': id, 'type' : type, 'index' : index});
-    console.log(favorite)
-    favorite.$save().then(function(){
-// $scope.applicants = Applicant.query();
-$scope.applicants.splice(index, 1);
-});
-};
-
-$scope.remove = function(id, index){
-    var place = new Applicant({'id' : id});
-    console.log(place);
-    place.$save().then(function(){
-        $scope.applicants.splice(0, 0, place);
-// $scope.applicants.splice(index, 1);
-});
-};
-
-$scope.button = function(text){
-    alert(text);
-}
-
-//code for toast messages
-$scope.showToast = function() {
-    console.log("hello");
-    document.getElementById("favToast").style.display = "block";
-    setTimeout($scope.hideToast, 3000);
-}
-
-$scope.hideToast = function() {
-    document.getElementById("favToast").style.display = "none";
-}
-
-// $scope.ret = function (id, type) {
-// 	var retObject = new RetObject({'id': id, 'type' : type});
-// 	retObject.$save();
-// }
-
-//scroll code
-$(function(){
-    var lastScrollTop = 0, delta = 5;
-    $(window).scroll(function(event){
-        var st = $(this).scrollTop();
-
-        if(Math.abs(lastScrollTop - st) <= delta)
-            return;
-
-        if (st > lastScrollTop){
-// downscroll code
-//console.log('scroll down');
-//console.log($(window).scrollTop());
-} else {
-// upscroll code
-// console.log('scroll up');
-// console.log($(window).scrollTop());
-angular.element("#footer").hide();
-}
-lastScrollTop = st;
-});
-});
-
-$(window).scroll(function() {   
-    if($(window).scrollTop() + $(window).height() == $(document).height()) {
-        angular.element("#footer").show();
-    }
-});
-}]);
+]);
