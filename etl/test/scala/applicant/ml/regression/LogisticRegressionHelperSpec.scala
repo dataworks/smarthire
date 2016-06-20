3d90cfed92f0cbcb1ab33e2bbb86387f863ad9c8
William Goubeaux <goubew@users.noreply.github.com>
2016-06-20 13:28:30 -0400

    Updated test case

--- etl/test/scala/applicant/ml/regression/LogisticRegressionHelperSpec.scala
+++ etl/test/scala/applicant/ml/regression/LogisticRegressionHelperSpec.scala
@@ -20,12 +20,12 @@ class LogisticRegressionHelperSpec extends FlatSpec with BeforeAndAfterAll {
    * Scala Test Spec to test the LogisticRegressionHelper object.
    */
 
-  val conf = new SparkConf().setMaster("local[*]").setAppName("ResumeReaderSpec")
+  val conf = new SparkConf().setMaster("local[1]").setAppName("ResumeReaderSpec")
   val sc = new SparkContext(conf)
   val simpleDataset: List[LabeledPoint] =
     List (
       LabeledPoint(1.0, Vectors.dense(1.0)),
-      LabeledPoint(0.0, Vectors.dense(0.0))
+      LabeledPoint(0.0, Vectors.dense(-1.0))
     ) //data has never been as statistically significant as this
 
    override def afterAll() = {
@@ -69,13 +69,9 @@ class LogisticRegressionHelperSpec extends FlatSpec with BeforeAndAfterAll {
 
     var testFeature: Double = 0.0
 
-    while (testFeature <= 1.0) {
-      println("The value at " + testFeature + " is " + LogisticRegressionHelper.predictSingleScore(model, Vectors.dense(testFeature)))
-      testFeature += 0.1
-    }
+    model.clearThreshold()
 
-    LogisticRegressionHelper.predictSingleScore(model, Vectors.dense(0.0)) mustBe 0.0
-    LogisticRegressionHelper.predictSingleScore(model, Vectors.dense(1.0)) mustBe 1.0
-    //LogisticRegressionHelper.predictSingleScore(model, Vectors.dense(0.5)) mustBe 0.5
+    LogisticRegressionHelper.predictSingleScore(model, Vectors.dense(-1.0)) must be < 0.5
+    LogisticRegressionHelper.predictSingleScore(model, Vectors.dense(1.0)) must be > 0.5
   }
 }
