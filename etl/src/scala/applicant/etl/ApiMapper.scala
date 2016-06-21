a2e00c41f4696cc175fc65090676fc4cae39101b
Brantley E. Gilbert <brantley.gilbert@dataworks-inc.com>
2016-06-21 09:19:54 -0400

    Added google maps api function as second ML feature, calculates distance between two points (cities. addresses, etc)

--- etl/src/scala/applicant/etl/ApiMapper.scala
+++ etl/src/scala/applicant/etl/ApiMapper.scala
@@ -48,13 +48,17 @@ object ApiMapper {
    * @param location2 Second location
    * @return Distance between the two locations in meters
    */
-  def googlemapsAPI(location1 : String, location2 : String) : Double = {
+  def googlemapsAPI(location1 : String, location2 : String) : Option[Int] = {
     val apiUrl = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + location1.trim().replaceAll("\\s", "+") + "&destinations=" + location2.trim().replaceAll("\\s", "+")
     val jsonString = scala.io.Source.fromURL(apiUrl).mkString
     println(jsonString)
-    val parsedJson = parse(jsonString)
+    val distance = ((((parse(jsonString) \ "rows")(0) \ "elements")(0) \ "distance") \ "value" )
     implicit val formats = DefaultFormats
-    println((((((parsedJson \ "rows")(0) \ "elements")(0))\ "distance") \ "value").extract[Double])// \ "elements")(0) \ "value")
-    return 0.0
+    if (distance.isInstanceOf[JInt]) {
+      return Some(distance.extract[Int])
+    }
+    else {
+      return None
+    }
   }
 }
