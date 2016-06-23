54fc825893d6c0abbb35328de27a8bd217ac495d
Alissa Cobb <acobb6@masonlive.gmu.edu>
2016-06-23 09:21:57 -0400

    returned to rescheck

--- app/client/app/components/applicants/applicantController.js
+++ app/client/app/components/applicants/applicantController.js
@@ -283,21 +283,6 @@ applicantControllers.controller('ApplicantCtrl', ['$scope', '$location', 'Applic
       }
     });
 
-    // function onSignIn(googleUser) {
-    //   var profile = googleUser.getBasicProfile();
-    //     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    //     console.log('Name: ' + profile.getName());
-    //     console.log('Image URL: ' + profile.getImageUrl());
-    //     console.log('Email: ' + profile.getEmail());
-    //   }
-
-    // function signOut() {
-    //   var auth2 = gapi.auth2.getAuthInstance();
-    //   auth2.signOut().then(function () {
-    //     console.log('User signed out.');
-    //   });
-    // }
-
   }]);
 
 
