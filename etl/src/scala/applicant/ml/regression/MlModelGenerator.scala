c8e07dd5c839de530019742a085a4b87e686fa5e
William Goubeaux <goubew@users.noreply.github.com>
2016-06-24 15:40:13 -0400

    Fixed printing for MlModelGenerator

--- etl/src/scala/applicant/ml/regression/MlModelGenerator.scala
+++ etl/src/scala/applicant/ml/regression/MlModelGenerator.scala
@@ -72,9 +72,9 @@ object MlModelGenerator {
     }
 
     //Create and save the logistic regression model
-    val model = LogisticRegressionHelper.createModel(sc, options.modelDirectory, modelData)
+    val model = LogisticRegressionHelper.createModel(sc, modelData)
 
-    println(model.weights())
+    println(model.weights)
 
     LogisticRegressionHelper.saveModel(model, sc, options.modelDirectory)
   }
