7cca1189ca612729e6cd64aa9e3065015f4c591a
William Goubeaux <goubew@users.noreply.github.com>
2016-06-30 16:47:53 -0400

    Added setup for NaiveBayes model creation

---
+++ etl/src/scala/applicant/ml/naivebayes/NaiveBayesFeatureGenerator.scala
@@ -0,0 +1,22 @@
+package applicant.ml.naivebayes
+
+import applicant.nlp.LuceneTokenizer
+import applicant.etl.ApplicantData
+import org.apache.spark.mllib.feature.HashingTF
+import org.apache.spark.mllib.linalg.{Vectors, Vector}
+
+object NaiveBayesFeatureGenerator {
+  val htf = new HashingTF(10000)
+  val tokenizer = new LuceneTokenizer()
+
+  /**
+   * Will return a vector of bucket size counds for the tokens from
+   *  the full body of the resume
+   *
+   * @param applicant The data for the current applicant
+   * @return A Vector of feature counts
+   */
+  def getFeatureVec(applicant: ApplicantData): Vector = {
+    return htf.transform(tokenizer.tokenize(applicant.fullText))
+  }
+}
