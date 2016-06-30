1e30685c7f601b06a10e78a2a71dc8c9271b42aa
William Goubeaux <goubew@users.noreply.github.com>
2016-06-30 13:02:16 -0400

    updated elasticsearch mapping script for applicants

--- etl/scripts/create-elasticsearch-mappings.sh
+++ etl/scripts/create-elasticsearch-mappings.sh
@@ -2,7 +2,27 @@
 
 #script to create elasticsearch index with desired mappings
 
-curl -XPUT 'http://interns.dataworks-inc.com/elasticsearch/applicants/applicant/_mapping' -d '
+curl -XPUT 'http://interns.dataworks-inc.com/elasticsearch/applicants/' -d '
+{
+  "settings": {
+    "index" : {
+        "number_of_shards" : 5,
+        "number_of_replicas" : 0
+    },
+    "analysis": {
+       "analyzer": {
+        "skills_analyzer": {
+          "type": "custom",
+          "tokenizer": "whitespace",
+          "filter": ["lowercase", "stop"]
+        }
+      }
+    }
+  }
+}
+'
+
+curl -XPUT 'http://interns.dataworks-inc.com/elasticsearch/applicants/_mapping/applicant/' -d '
 {
   "applicant": {
     "properties": {
@@ -13,6 +33,34 @@ curl -XPUT 'http://interns.dataworks-inc.com/elasticsearch/applicants/applicant/
           "raw": {
             "type": "string",
             "index": "not_analyzed"
+          }
+        }
+      },
+      "skills": {
+        "properties": {
+          "bigdata": {
+            "type": "string",
+            "analyzer": "skills_analyzer"
+          },
+          "database": {
+            "type": "string",
+            "analyzer": "skills_analyzer"
+          },
+          "etl": {
+            "type": "string",
+            "analyzer": "skills_analyzer"
+          },
+          "language": {
+            "type": "string",
+            "analyzer": "skills_analyzer"
+          },
+          "mobile": {
+            "type": "string",
+            "analyzer": "skills_analyzer"
+          },
+          "webapp": {
+            "type": "string",
+            "analyzer": "skills_analyzer"
           }
         }
       }
