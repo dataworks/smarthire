6aa14ef8e5308e76b5d08463483a66d22fce8363
Dennis L. Huang <dlh4fx@virginia.edu>
2016-07-25 14:08:54 -0400

    started to reformat admin page

--- app/client/app/components/admin/adminController.js
+++ app/client/app/components/admin/adminController.js
@@ -3,5 +3,48 @@ applicantControllers.controller('AdminCtrl', ['$scope', 'Admin',
 
   	$scope.settings = Admin.query();
 
+  	var vm = this;
+
+  	// The model object that we reference
+    // on the  element in index.html
+    vm.rental = {};
+    
+    // An array of our form fields with configuration
+    // and options set. We make reference to this in
+    // the 'fields' attribute on the  element
+    vm.rentalFields = [
+        {
+            key: 'first_name',
+            type: 'input',
+            templateOptions: {
+                type: 'text',
+                label: 'First Name',
+                placeholder: 'Enter your first name',
+                required: true
+            }
+        },
+        {
+            key: 'last_name',
+            type: 'input',
+            templateOptions: {
+                type: 'text',
+                label: 'Last Name',
+                placeholder: 'Enter your last name',
+                required: true
+            }
+        },
+        {
+            key: 'email',
+            type: 'input',
+            templateOptions: {
+                type: 'email',
+                label: 'Email address',
+                placeholder: 'Enter email',
+                required: true
+            }
+        },
+    ];
+    
+
   }
 ]);
